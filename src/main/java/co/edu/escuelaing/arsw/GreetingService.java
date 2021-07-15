4ae1ae9f3d442d02e844dd4dfd89c215adad208b
Maria Camila Fetecua <maria.fetecua@mail.escuelaing.edu.co>
2021-07-15 10:18:41 -0500

    Quiz terminado

---
+++ src/main/java/co/edu/escuelaing/arsw/GreetingService.java
@@ -0,0 +1,11 @@
+package co.edu.escuelaing.arsw;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+    public String greet() {
+        return "Hello, World";
+    }
+}
---
+++ src/test/java/co/edu/escuelaing/arsw/TestingWebApplicationTests.java
@@ -0,0 +1,14 @@
+package co.edu.escuelaing.arsw;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestingWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
